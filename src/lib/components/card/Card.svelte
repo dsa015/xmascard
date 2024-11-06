1aa77116811ff70b2da9af8b606f0cb1ce52f1a8
Diluxan Sathalingam <diluxan.sathalingam@dnb.no>
2024-11-06 15:43:36 +0100

    some cool titing card effect

---
+++ src/lib/components/card/Card.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	export let title;
+	export let message;
+
+	let rotateX = 0;
+	let rotateY = 0;
+	const tiltDegree = 20;
+
+	const handleMouseMove = (event: MouseEvent) => {
+		// Dimensions of the card
+		const centerX = window.innerWidth / 2;
+		const centerY = window.innerHeight / 2;
+
+		// Calculate where to tilt
+		rotateX = ((event.clientY - centerY) / centerY) * tiltDegree;
+		rotateY = ((event.clientX - centerX) / centerX) * tiltDegree;
+	};
+</script>
+
+<section aria-label="img" on:mousemove={handleMouseMove}>
+	<div style="transform: rotateX({rotateX}deg) rotateY({rotateY}deg);">
+		<h1>{title}</h1>
+		<p>{message}</p>
+	</div>
+</section>
+
+<style>
+	section {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+		width: 100%;
+		perspective: 1000px;
+	}
+
+	div {
+		color: black;
+		background-color: #f9f9f9;
+		border-radius: 1rem;
+		padding: 2em;
+	}
+</style>
