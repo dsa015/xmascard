024099bb40f266f0300a4663c0ad599542a455a9
celineSeland <celine.seland@outlook.com>
2024-11-06 16:11:31 +0100

    envelope first edition (#6)
    Co-authored-by: Celine <celine.habbestad.seland@dnb.no>

--- src/routes/(xmascard)/[card=cardParam]/+page.svelte
+++ src/routes/(xmascard)/[card=cardParam]/+page.svelte
@@ -1,6 +1,25 @@
 <script>
 	import Card from '$lib/components/card/Card.svelte';
+	import Envelope from '$lib/components/Envelope.svelte';
 	export let data;
 </script>
 
+<div class="container">
 <Card title={data.title} message={data.message} visits={data.views} />
+	</Envelope>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		text-align: center;
+	}
+
+	Envelope {
+		max-width: 400px;
+		padding: 200px;
+	}
+</style>
